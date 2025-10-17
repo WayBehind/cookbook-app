8993c272ca81d7b64e2d8194d992a977544efbb0
WayBehind <mgromoss@gmail.com>
2025-10-17 12:17:27 +0200

    first commit

---
+++ src/test/java/sk/waybehind/cookbook_app/CookbookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.waybehind.cookbook_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
