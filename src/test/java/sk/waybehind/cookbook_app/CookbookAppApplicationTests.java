686b68ec172d308258a75693a31dbdbdc86d403a
WayBehind <mgromoss@gmail.com>
2025-10-20 16:49:40 +0200

    Add getAll test - base for future tests

--- src/test/java/sk/waybehind/cookbook_app/CookbookAppApplicationTests.java
+++ src/test/java/sk/waybehind/cookbook_app/CookbookAppApplicationTests.java
@@ -1,13 +1,40 @@
 package sk.waybehind.cookbook_app;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import sk.waybehind.cookbook_app.domain.Recipe;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CookbookAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void getAll() {
+        final ResponseEntity<List<Recipe>> responseEntity = testRestTemplate.exchange(
+                "/api/recipes",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Recipe>>() {
+                }
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertTrue(responseEntity.getBody().size() >= 2);
+
+    }
 
 }
